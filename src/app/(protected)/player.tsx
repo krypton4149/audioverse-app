f10b73f89f80921db6fe80abe042bdba1d88252c
krypton414 <piyush.yadav@webnotics.org>
2025-07-04 18:27:28 +0530

    Player Change Position (SeekTo)

--- src/app/(protected)/player.tsx
+++ src/app/(protected)/player.tsx
@@ -4,15 +4,20 @@ import { router } from "expo-router";
 import dummyBooks from "@/dummyBooks";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PlaybackBar from "@/components/PlaybackBar";
-import { useAudioPlayer } from "expo-audio";
+import { useAudioPlayer, useAudioPlayerStatus } from "expo-audio";
 
 export default function PlayerScreen() {
   const book = dummyBooks[0];
 
   const player = useAudioPlayer(book.audio_url);
-        
+  const playerStatus = useAudioPlayerStatus(player);
+
+  const seekTo = (time: number) => {
+    player.seekTo(time);
+  };
+
   return (
-    <SafeAreaView className="flex-1 py-10 bg-gray-950 p-4 gap-4">
+    <SafeAreaView className="flex-1 py-10  p-4 gap-4">
       <Pressable
         onPress={() => router.back()}
         className="bg-gray-800 p-2 rounded-full left-4 top-16 absolute"
@@ -30,12 +35,23 @@ export default function PlayerScreen() {
         </Text>
         <Text className="text-gray-400 text-center text-sm">{book.author}</Text>
 
-        <PlaybackBar value={0.5} />
+        <PlaybackBar
+          currentTime={playerStatus.currentTime}
+          duration={playerStatus.duration}
+          onSeek={seekTo}
+        />
 
         <View className="flex-row items-center justify-between ">
           <Ionicons name="play-skip-back" size={24} color="white" />
           <Ionicons name="play-back" size={24} color="white" />
-          <Ionicons name="play" size={50} color="white" />
+          <Ionicons
+            onPress={() =>
+              playerStatus.playing ? player.pause() : player.play()
+            }
+            name={playerStatus.playing ? "pause" : "play"}
+            size={50}
+            color="white"
+          />
           <Ionicons name="play-forward" size={24} color="white" />
           <Ionicons name="play-skip-forward" size={24} color="white" />
         </View>
